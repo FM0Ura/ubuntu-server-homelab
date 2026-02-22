83ed033aff32c5b2694e0df38270f3a52d1015a1
Felipe Angelo Zucolotto Moura <fe.angelo@outlook.com>
2026-02-22 12:58:07 -0300

    Add installation script for CasaOS

---
+++ install_casaos.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "================================================================"
+echo "                 Instalando o CasaOS                            "
+echo "================================================================"
+
+# O CasaOS tem um script de instalação oficial que já configura tudo
+curl -fsSL https://get.casaos.io | sudo bash
+
+echo "================================================================"
+echo " CasaOS Instalado! Acesse pelo navegador usando o IP do servidor"
+echo "================================================================"
